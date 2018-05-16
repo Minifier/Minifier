b78c30d0f570712a120e8731b6c80da41e6dc815
Oscar MARIE--TAILLEFER <o5mariet@enib.fr>
2018-05-16 15:43:23 +0200

    Factorisation du compress File/ CSS / JS
    * Factorisation du compressfile/ CSS / JS
    
    * Delete compressjs.cpp
    
    * Delete compressjs.h
    
    * Delete compresscss.cpp
    
    * Delete compresscss.h
    
    * Update compressfile.h
    
    * Update Minifier.pro
    
    * Update codescompressor.h
    
    * Update codescompressor.cpp

--- Minifier/compressfile.h
+++ Minifier/compressfile.h
@@ -1,21 +1,18 @@
+
 /*******************************************************************
 Copyright (C) 2018 Oscar MARIE--TAILLEFER   <o5mariet@enib.fr>
 Copyright (C) 2018 Nils JEGOU-GERGAUD       <n5jegoug@enib.fr>
 Copyright (C) 2018 Nicols FRANCIS           <n4franci@enib.fr>
 Copyright (C) 2018 Corentin LAMBERT         <c4lamber@enib.fr>
-
 This file is part of Minifier.
-
 Minifier is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
 the Free Software Foundation, either version 3 of the License, or
 (at your option) any later version.
-
 Minifier is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 GNU General Public License for more details.
-
 You should have received a copy of the GNU General Public License
 along with Minifier.  If not, see <http://www.gnu.org/licenses/>.
 *******************************************************************/
@@ -25,8 +22,11 @@ along with Minifier.  If not, see <http://www.gnu.org/licenses/>.
 
 #include <QObject>
 #include <QString>
+#include <Qfilesystemwatcher>
+#include <QProcess>
 
 #include "utils.h"
+#include "config.h"
 
 namespace code_compressor{
 
@@ -39,63 +39,32 @@ namespace code_compressor{
         CompressFile(const CompressFile &) = delete;
         CompressFile &operator=(CompressFile &&) = delete;
         CompressFile &operator=(const CompressFile &) = default;
-        virtual ~CompressFile() {};
+        ~CompressFile();
 
-        virtual void compress();
-        virtual void stop();
+        void compress();
+        void stop();
 
         // Accesseur
-        inline bool getChange();
-        inline bool getExist();
         inline QString getFilePath();
         inline QString getOutputFile();
-        inline QString getExePath();
 
         // Mutateur
-        inline void setChange(bool change);
-        inline void setExist(bool exist);
         inline void setFilePath(const QString &filePath);
         inline void setOutputFile(const QString &output);
-        inline void setExePath(const QString &exePath);
+        
+        QFileSystemWatcher* fileWatch;
+
+    public slots:
+        UINT CompressCallback();
 
     protected:
-        QString _exePath, _filePath, _outputFile;
-        bool _change, _exist;
+        QString _filePath, _outputFile;
     };
-
-    /**
-    * @brief  void function
-    * @note
-    * @retval None
-    */
-    inline void CompressFile::compress()
-    {
-        throw "Not implemented";
-    }
-
-
-    /**
-    * @brief  void function
-    * @note
-    * @retval None
-    */
-    inline void CompressFile::stop()
-    {
-        throw "Not implemented";
-    }
 
     /*******************************************/
     /************* Acesseur ********************/
     /*******************************************/
 
-    inline bool CompressFile::getChange()
-    {
-        return this->_change;
-    }
-    inline bool CompressFile::getExist()
-    {
-        return this->_exist;
-    }
     inline QString CompressFile::getFilePath()
     {
         return this->_filePath;
@@ -105,23 +74,10 @@ namespace code_compressor{
         return this->_outputFile;
     }
 
-    inline QString CompressFile::getExePath()
-    {
-        return this->_exePath;
-    }
-
     /*******************************************/
     /************* Mutateur ********************/
     /*******************************************/
 
-    inline void CompressFile::setChange(bool change)
-    {
-        this->_change = change;
-    }
-    inline void CompressFile::setExist(bool exist)
-    {
-        this->_exist = exist;
-    }
     inline void CompressFile::setFilePath(const QString &filePath)
     {
         this->_filePath = filePath;
@@ -130,11 +86,6 @@ namespace code_compressor{
     inline void CompressFile::setOutputFile(const QString &output)
     {
         this->_outputFile = output;
-    }
-
-    inline void CompressFile::setExePath(const QString &exePath)
-    {
-        this->_exePath = exePath;
     }
 
 } //end code_compressor
